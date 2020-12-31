902d859cb846d33ebcc4c54d177594008f5c5599
He Hao <hehao@microsoft.com>
2020-12-31 11:06:21 -0800

    added fixed_window_throttler_test.py

--- tests/fixed_window_throttler_test.py
+++ tests/fixed_window_throttler_test.py
@@ -1,10 +1,27 @@
+import time
 from unittest import TestCase
-from unittest.mock import patch, MagicMock
 
 from api_throttler import FixedWindowThrottler
 
 
 class TestFixedWindowThrottler(TestCase):
 
-    def test_dummy(self):
-        self.assertEqual(True, True)
+    def setUp(self) -> None:
+        self.throttler = FixedWindowThrottler(2, 5)
+
+    def test_continuous_calls(self):
+        allowed_calls = 0
+        for i in range(5):
+            if not self.throttler.is_throttled(key="test_key"):
+                allowed_calls += 1
+            time.sleep(1)
+        self.assertEqual(allowed_calls, 2)
+
+    def test_periodic_calls(self):
+        allowed_calls = 0
+        for i in range(10):
+            if i in {0, 4, 5, 6}:
+                if not self.throttler.is_throttled(key="test_key"):
+                    allowed_calls += 1
+            time.sleep(1)
+        self.assertEqual(allowed_calls, 4)
