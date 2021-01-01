f1fb8cba39a5ef9174c8f7bc643534f3595a3e9e
He Hao <hehao@microsoft.com>
2020-12-31 16:50:24 -0800

    added fixed and sliding window throttlers into __init__.py

--- api_throttler/__init__.py
+++ api_throttler/__init__.py
@@ -3,3 +3,5 @@ __version__ = '0.1.0'
 from .throttler import Throttler
 from .fixed_window_throttler import FixedWindowThrottler
 from .sliding_window_throttler import SlidingWindowThrottler
+from .fixed_window_throttler_redis import FixedWindowThrottlerRedis
+from .sliding_window_throttler_redis import SlidingWindowThrottlerRedis
